a7fbc53a281a2168120a895f1ae3e7c47c74cda7
Sea Grapes <natuloid@gmail.com>
2024-10-05 10:51:41 -0700

    setup

---
+++ src/routes/(app)/+page.svelte
@@ -0,0 +1,22 @@
+<svelte:head>
+  <title>Orrery</title>
+</svelte:head>
+
+<script>
+import Scene from './Scene.svelte';
+</script>
+
+<canvas></canvas>
+
+<style>
+:global(html, body) {
+  height: 100%;
+  overflow: hidden;
+}
+
+canvas {
+  display: block;
+  width: 100%;
+  height: 100%;
+}
+</style>
